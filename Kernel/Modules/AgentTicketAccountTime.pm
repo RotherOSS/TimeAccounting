40144c5291d90014b5a1821eaacd03fff5f7bdd8
Sven <svo@otobo.org>
2025-05-29 19:06:49 +0200

    Optionally account the time for the first article.

--- Kernel/Modules/AgentTicketAccountTime.pm
+++ Kernel/Modules/AgentTicketAccountTime.pm
@@ -189,6 +189,33 @@ sub Run {
             );
         }
 
+        my $Config = $ConfigObject->Get('Ticket::Frontend::AgentTicketAccountTime') // {};
+
+        if ( $Config->{AccountTimeOnFirstArticle} ) {
+            my @Articles = $Kernel::OM->Get('Kernel::System::Ticket::Article')->ArticleList(
+                TicketID  => $TicketID,
+                OnlyFirst => 1,
+            );
+
+            if ( @Articles ) {
+                $Success = $TicketObject->TicketAccountTime(
+                    TicketID  => $TicketID,
+                    ArticleID => $Articles[0]{ArticleID},
+                    TimeUnit  => $GetParam{Period},
+                    UserID    => $Self->{UserID},
+                );
+            }
+
+            if ( !@Articles || !$Success ) {
+                $Kernel::OM->Get('Kernel::System::Log')->Log(
+                    Priority => 'error',
+                    Message  => "Could account time (" . $ParamObject->GetParam( Param => 'Period' )
+                        . ") on first article of ticket (TicketID: " . $ParamObject->GetParam( Param => 'TicketID' )
+                        . ") for UserID $Self->{UserID}."
+                );
+            }
+        }
+
         # challenge token check for write action
         $LayoutObject->ChallengeTokenCheck();
         return $LayoutObject->PopupClose(
